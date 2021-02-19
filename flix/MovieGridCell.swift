0f5f9586daaf129a47167d88f892eca5741fa3eb
Mehbub Rohit <mehburohit@gmail.com>
2021-02-19 02:03:47 -0500

    part 2 of flix

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Mehbub Rohit on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
